b080ab2e0aeadc3ce329869368885f4eda8f65fd
GeorgeTYQ <157372345+GeorgTYQ@users.noreply.github.com>
2025-07-08 13:31:37 +1000

    chore( init project)

---
+++ index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import APP from "@/App";
+
+const rootDOM = ReactDOM.createRoot(document.getElementById("root"));
+
+
+rootDOM.render(
+  <APP />
+)
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const APP = () => {
+  return (
+    <div>app</div>
+  )
+}
